6c3bc563825921e260c429afe9bc3b57f9b9ee7b
unknown <elorenzo@pronos.cl>
2018-11-13 16:42:38 -0300

    primer commit real

---
+++ codigo/Gema_Curricular/Entidades/Perfil_egreso.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+
+namespace Gema_Curricular.Entidades 
+{
+    public class Perfil_egreso()
+    {
+        public string Nombre;
+        public string Descripcion;
+        public List<string> Lista_ambitos_desempeño;
+        public List<Categoria> Lista_competencias;
+
+        public Perfil_egreso(string nombre, string descripcion)
+        {
+            this.Nombre = nombre;
+            this.Descripcion = descripcion;
+            this.Lista_ambitos_desempeño = new List<string>();
+            this.Lista_competencias = new List<Categoria>();
+        }
+    }
+}
