9f3515913c52cb852d4a09fd9e1a89bcd62230b3
unknown <elorenzo@pronos.cl>
2018-11-15 11:12:08 -0300

    avance del trabajo

--- codigo/Gema_Curricular/Entidades/Perfil_egreso.cs
+++ codigo/Gema_Curricular/Entidades/Perfil_egreso.cs
@@ -3,17 +3,21 @@
 
 namespace Gema_Curricular.Entidades 
 {
-    public class Perfil_egreso()
+    public class Perfil_egreso
     {
+        public int ID;
         public string Nombre;
         public string Descripcion;
+        public float Peso;
         public List<string> Lista_ambitos_desempeño;
         public List<Categoria> Lista_competencias;
 
-        public Perfil_egreso(string nombre, string descripcion)
+        public Perfil_egreso(int id, string nombre, string descripcion, float peso)
         {
+            this.ID = id;
             this.Nombre = nombre;
             this.Descripcion = descripcion;
+            this.Peso = peso;
             this.Lista_ambitos_desempeño = new List<string>();
             this.Lista_competencias = new List<Categoria>();
         }
