8ac31cfced5e7ccc9134007a26c27c82b44aca92
unknown <elorenzo@pronos.cl>
2018-12-06 16:26:30 -0300

    1

---
+++ codigo/Gema_Curricular/modulos/evaluaciones/entidades/Pauta.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace gema_curricular_evaluaciones.entidades
+{
+    public class Pauta
+    {
+        
+    }
+}
